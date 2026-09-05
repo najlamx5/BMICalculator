e6595b8d4582f0f20f9770d0c7a3677b45a0c219
najlamx5 <najlamx5@gmail.com>
2026-09-05 23:39:52 +0300

    Fix code block formatting in app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-```python
+#```python
 import re
 import streamlit as st
 from PIL import Image, ImageOps, ImageEnhance, ImageFilter
@@ -1213,4 +1213,4 @@ st.markdown(
     """,
     unsafe_allow_html=True
 )
-```
+
